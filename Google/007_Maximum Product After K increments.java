17991d39ee744f742e6a6202b9f2e357f4187367
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-02-01 20:21:26 +0530

    Add files via upload

---
+++ Google/007_Maximum Product After K increments.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution{
+    public int maximumProduct(int[] nums, int k) {
+            int n=nums.length;
+            long mod=1000000007;
+            if(n==1){
+                long ans=(nums[0]+k)%mod;
+                return (int)ans;
+            }
+            PriorityQueue<Long> pq=new PriorityQueue<>();
+            for(int a:nums){
+                pq.add((long)a);
+            }
+            while(k>0){
+                long num1=pq.remove();
+                long num2=pq.remove();
+                long diff=Math.min(k,(num2-num1)+1);
+                num1+=diff;
+                k-=diff;
+                pq.add(num1);
+                pq.add(num2);
+            }
+            long ans=1;
+            while(!pq.isEmpty()){
+                ans=(ans*pq.remove())%mod;
+            }
+            return (int)ans;
+        }
+    }
