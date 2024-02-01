74d8b7291d4d116fd07f5ce99221c82bbad59e2e
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-02-01 20:19:43 +0530

    Add files via upload

---
+++ Google/001_Destroying Asteroids.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long m=mass;
+        for(final int asteroid:asteroids)
+        if(m>=asteroid)
+        m+=asteroid;
+        else
+        return false;
+        return true;
+    }
+}
